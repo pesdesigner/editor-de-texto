4b51499b94df2c6685cd4800f19d2898a370336b
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2020-04-20 00:07:36 -0300

    Métodos - Font Bold | Italic | Underline

--- Form1.cs
+++ Form1.cs
@@ -108,5 +108,212 @@ namespace Editor_de_Texto
         {
             Abrir();
         }
+        private void Copiar()
+        {
+            if (richTextBox1.SelectionLength > 0)
+            {
+                richTextBox1.Copy();
+            }
+        }
+        private void Colar()
+        {
+            richTextBox1.Paste();
+        }
+
+        private void btn_copiar_Click(object sender, EventArgs e)
+        {
+            Copiar();
+        }
+
+        private void btn_colar_Click(object sender, EventArgs e)
+        {
+            Colar();
+        }
+
+        private void copiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Copiar();
+        }
+
+        private void colarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Colar();
+        }
+        private void FontBold()
+        {
+            string fontName = null;
+            float fontSize = 0;
+            bool b,i,u = false;
+
+            fontName = richTextBox1.Font.Name;
+            fontSize = richTextBox1.Font.Size;
+            b = richTextBox1.SelectionFont.Bold;
+            i = richTextBox1.SelectionFont.Italic;
+            u = richTextBox1.SelectionFont.Underline;
+
+            richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Regular);
+            if (b == false)
+            {
+                if (i == true & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline);
+                }
+                else if(i == false & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Underline);
+                }
+                else if (i == true & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic);
+                }
+                else if (i == false & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold);
+                }
+            }
+            else
+            {
+                if (i == true & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic | FontStyle.Underline);
+                }
+                else if (i == false & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Underline);
+                }
+                else if(i == true & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic);
+                }
+            }
+
+        }
+        private void FontItalic()
+        {
+            string fontName = null;
+            float fontSize = 0;
+            bool b, i, u = false;
+
+            fontName = richTextBox1.Font.Name;
+            fontSize = richTextBox1.Font.Size;
+            b = richTextBox1.SelectionFont.Bold;
+            i = richTextBox1.SelectionFont.Italic;
+            u = richTextBox1.SelectionFont.Underline;
+
+            richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Regular);
+            if (i == false)
+            {
+                if (b == true & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline);
+                }
+                else if (b == false & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic | FontStyle.Underline);
+                }
+                else if (b == true & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic);
+                }
+                else if (b == false & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic);
+                }
+            }
+            else
+            {
+                if (b == true & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Underline);
+                }
+                else if (i == false & u == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Underline);
+                }
+                else if (i == true & u == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold);
+                }
+            }
+
+        }
+        private void FontUnderline()
+        {
+            string fontName = null;
+            float fontSize = 0;
+            bool b, i, u = false;
+
+            fontName = richTextBox1.Font.Name;
+            fontSize = richTextBox1.Font.Size;
+            b = richTextBox1.SelectionFont.Bold;
+            i = richTextBox1.SelectionFont.Italic;
+            u = richTextBox1.SelectionFont.Underline;
+
+            richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Regular);
+            if (u == false)
+            {
+                if (b == true & i == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline);
+                }
+                else if (b == false & i == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic | FontStyle.Underline);
+                }
+                else if (b == true & i == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Underline);
+                }
+                else if (b == false & i == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Underline);
+                }
+            }
+            else
+            {
+                if (b == true & i == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold | FontStyle.Italic);
+                }
+                else if (b == false & i == true)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Italic);
+                }
+                else if (b == true & i == false)
+                {
+                    richTextBox1.SelectionFont = new Font(fontName, fontSize, FontStyle.Bold);
+                }
+            }
+        }
+
+        private void btn_negrito_Click(object sender, EventArgs e)
+        {
+            FontBold();
+        }
+
+        private void btn_italico_Click(object sender, EventArgs e)
+        {
+            FontItalic();
+        }
+
+        private void btn_sublinhado_Click(object sender, EventArgs e)
+        {
+            FontUnderline();
+        }
+
+        private void negritoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontBold();
+        }
+
+        private void itálicoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontItalic();
+        }
+
+        private void sublinhadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontUnderline();
+        }
     }
 }
